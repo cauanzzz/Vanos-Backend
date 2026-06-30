294326bbdd33d45853f1f6b23f0e32df31756dd6
Cauan Braga <cauanbraga7@gmail.com>
2026-06-30 15:28:54 -0300

    feat: add School and MonthlyFee entities for routing and financial dashboard

---
+++ Vanos.API/Models/MonthlyFee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Vanos.API.Models
+{
+    public class MonthlyFee
+    {
+        public int Id { get; set; }
+
+        public int StudentId { get; set; }
+        public int DriverId { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime DueDate { get; set; } 
+        public bool IsPaid { get; set; } = false; 
+        public DateTime? PaymentDate { get; set; } 
+    }
+}
