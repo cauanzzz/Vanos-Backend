791b15085c24fc9ba4acbdf31f9d504329bff47c
Cauan Braga <cauanbraga7@gmail.com>
2026-06-30 15:13:38 -0300

    feat: add Driver entity model

---
+++ Vanos.API/Models/Driver.cs
@@ -0,0 +1,15 @@
+﻿namespace Vanos.API.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string LicensePlate { get; set; } = string.Empty;
+        public int StudentCapacity { get; set; }
+        public string PixKey { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+
+    }
+}
