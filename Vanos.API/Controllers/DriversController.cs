9e1e9e53a9537ca9c8ddd5033df5f8fabfbe1a6e
Cauan Braga <cauanbraga7@gmail.com>
2026-06-30 15:59:04 -0300

    feat: add DriversController and configure API routing

---
+++ Vanos.API/Controllers/DriversController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vanos.API.Data;
+using Vanos.API.Models;
+
+namespace Vanos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriversController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DriversController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Driver>>> GetDrivers()
+        {
+            return await _context.Drivers.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Driver>> PostDriver(Driver driver)
+        {
+            _context.Drivers.Add(driver);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetDrivers), new { id = driver.Id }, driver);
+        }
+    }
+}
---
+++ Vanos.API/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vanos.API.Models;
+
+namespace Vanos.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<School> Schools { get; set; }
+        public DbSet<MonthlyFee> MonthlyFees { get; set; }
+    }
+}
