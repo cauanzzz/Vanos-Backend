4af053b20e5006c57eb909d9a208d39d48525803
Cauan Braga <cauanbraga7@gmail.com>
2026-07-06 15:28:37 -0300

    feat: add Schools and MonthlyFees controllers

---
+++ Vanos.API/Controllers/MonthlyFeesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vanos.API.Data;
+using Vanos.API.Models;
+
+namespace Vanos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonthlyFeesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public MonthlyFeesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MonthlyFee>>> GetMonthlyFees()
+        {
+            return await _context.MonthlyFees.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MonthlyFee>> PostMonthlyFee(MonthlyFee monthlyFee)
+        {
+            var studentExists = await _context.Students.AnyAsync(s => s.Id == monthlyFee.StudentId);
+            var driverExists = await _context.Drivers.AnyAsync(d => d.Id == monthlyFee.DriverId);
+
+            if (!studentExists || !driverExists)
+            {
+                return BadRequest("Aluno ou Motorista não encontrado. Verifique os IDs repassados.");
+            }
+
+            _context.MonthlyFees.Add(monthlyFee);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMonthlyFees), new { id = monthlyFee.Id }, monthlyFee);
+        }
+    }
+}
---
+++ Vanos.API/Controllers/SchoolsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Vanos.API.Data;
+using Vanos.API.Models;
+
+namespace Vanos.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SchoolsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public SchoolsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<School>>> GetSchools()
+        {
+            return await _context.Schools.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<School>> PostSchool(School school)
+        {
+            _context.Schools.Add(school);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSchools), new { id = school.Id }, school);
+        }
+    }
+}
